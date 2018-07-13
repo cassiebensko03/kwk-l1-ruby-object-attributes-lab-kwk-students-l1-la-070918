0dbe68d5cf89b3a47690aef00feabf5841e7ea5a
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-13 16:51:08 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,18 @@ class Dog
   attr_accessor :name, :breed
   def initializer(name, breed)
     
-@name=
+@name=name
+
+@breed=breed
+end
+end
+
+def breeds
+  if @breed == "default"
+    breeds = "Mutt"
+  end
+end
+
+dog_one=Dog.new("Stan","default")
+
+puts dog_one.name
